66b9aef50f94dfbb32a3ee0f6f2860f16b28ecb1
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-26 15:28:49 -0300

    feat: Implementando rotas na main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:info_libs/app/routes/app_pages.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      title: 'Info Libs',
+      initialRoute: Routes.home,
+      getPages: AppPages.pages,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
