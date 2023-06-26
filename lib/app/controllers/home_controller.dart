551283e6188ed1342e041f6780e381900f91e42d
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-26 15:59:23 -0300

    feat: Coletando informações do aplicativo

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,5 +1,22 @@
 import 'package:get/get.dart';
 
+import 'package:package_info_plus/package_info_plus.dart';
+
 class HomeController extends GetxController {
   HomeController();
+
+  //Info app
+  String appName = '';
+  String packageName = '';
+  String version = '';
+  String buildNumber = '';
+
+  Future<void> fetchInfoApp() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    appName = packageInfo.appName;
+    packageName = packageInfo.packageName;
+    version = packageInfo.version;
+    buildNumber = packageInfo.buildNumber;
+  }
 }
