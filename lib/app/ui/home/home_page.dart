f58ef80ef453aeb11334d336f2b6cbd862d43bf8
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-27 11:05:53 -0300

    feat: Criando card de informaçoes do aplicativo

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -8,6 +8,53 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: Text('Home Page')));
+    return Scaffold(body: GetBuilder<HomeController>(builder: (_) {
+      return SafeArea(
+          child: ListView(padding: const EdgeInsets.all(10), children: [
+        Container(
+            decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.black)),
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Center(
+                    child: Text("Informações do aplicativo",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                SizedBox(height: 10),
+                TextInfo(label: 'Nome', data: _.appName),
+                TextInfo(label: 'Nome do aplicativo', data: _.packageName),
+                TextInfo(label: 'Versão', data: _.version),
+                TextInfo(label: 'Numero do build', data: _.buildNumber),
+                TextInfo(label: 'Assinatura do build', data: _.buildSignature),
+                TextInfo(
+                    label: 'Loja que foi instalado',
+                    data: '${_.installerStore}'),
+              ],
+            ))
+      ]));
+    }));
+  }
+}
+
+class TextInfo extends StatelessWidget {
+  const TextInfo({super.key, required this.label, required this.data});
+
+  final String label;
+  final String data;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomeController>(
+      builder: (_) {
+        return Text(
+          "$label: $data",
+          style: const TextStyle(
+            fontSize: 16,
+          ),
+        );
+      },
+    );
   }
 }
