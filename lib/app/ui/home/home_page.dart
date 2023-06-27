7832a3df3716eebaa13ceeaacdd7dad8afd1cd2e
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-27 11:20:45 -0300

    feat: Componentizando card de informaçoes

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -10,39 +10,72 @@ class HomePage extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(body: GetBuilder<HomeController>(builder: (_) {
       return SafeArea(
-          child: ListView(padding: const EdgeInsets.all(10), children: [
-        Container(
+          child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(10),
+              children: [
+            CardInfo(title: 'Informações do aplicativo', data: [
+              {'label': 'Nome', 'data': _.appName},
+              {'label': 'Nome do aplicativo', 'data': _.packageName},
+              {'label': 'Versão', 'data': _.version},
+              {'label': 'Numero do build', 'data': _.buildNumber},
+              {'label': 'Assinatura do build', 'data': _.buildSignature},
+              {
+                'label': 'Loja que foi instalado',
+                'data': '${_.installerStore}'
+              },
+            ])
+          ]));
+    }));
+  }
+}
+
+class CardInfo extends StatelessWidget {
+  const CardInfo({super.key, required this.data, required this.title});
+
+  final String title;
+  final List<Map<String, String>> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomeController>(
+      builder: (_) {
+        return Container(
             decoration: BoxDecoration(
                 border: Border.all(width: 1, color: Colors.black)),
             padding: const EdgeInsets.all(10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Center(
-                    child: Text("Informações do aplicativo",
-                        style: TextStyle(
+                Center(
+                    child: Text(title,
+                        style: const TextStyle(
                             fontSize: 18, fontWeight: FontWeight.bold))),
-                SizedBox(height: 10),
-                TextInfo(label: 'Nome', data: _.appName),
-                TextInfo(label: 'Nome do aplicativo', data: _.packageName),
-                TextInfo(label: 'Versão', data: _.version),
-                TextInfo(label: 'Numero do build', data: _.buildNumber),
-                TextInfo(label: 'Assinatura do build', data: _.buildSignature),
-                TextInfo(
-                    label: 'Loja que foi instalado',
-                    data: '${_.installerStore}'),
+                const SizedBox(height: 10),
+                ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 5);
+                    },
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      return TextInfo(
+                          label: data[index]['label'],
+                          data: data[index]['data']);
+                    }),
               ],
-            ))
-      ]));
-    }));
+            ));
+      },
+    );
   }
 }
 
 class TextInfo extends StatelessWidget {
   const TextInfo({super.key, required this.label, required this.data});
 
-  final String label;
-  final String data;
+  final String? label;
+  final String? data;
 
   @override
   Widget build(BuildContext context) {
